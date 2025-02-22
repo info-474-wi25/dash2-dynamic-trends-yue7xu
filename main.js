4a82f5a3872bbe37aed54b55a331c74da78fb08e
yue7xu <luckyyue@uw.edu>
2025-02-21 23:39:27 -0800

    Classwork 7 - Code Set-up for Dash 2, Upload and csv file, transform data.

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const width = 900 - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom;
 
 // Create SVG containers for both charts
-const svg1_RENAME = d3.select("#lineChart1") // If you change this ID, you must change it in index.html too
+const svgLine = d3.select("#lineChart1") // If you change this ID, you must change it in index.html too
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -22,12 +22,42 @@ const svg2_RENAME = d3.select("#lineChart2")
 // const tooltip = ...
 
 // 2.a: LOAD...
-d3.csv("YOUR_CSV_NAME.csv").then(data => {
+d3.csv("aircraft_incidents.csv").then(data => {
     // 2.b: ... AND TRANSFORM DATA
+    data.forEach(d => {
+        d["year"] = +d["Year"];
+    });
+
+    console.log("Raw data:", data);
+    console.log("Years:", data.map(d => d.year));
+
+    // 2.c: PREPARE DATA - Categorize Weather Conditions into Known Groups
+    const weatherCategories = ["VMC", "IMC", "NA"];
+    const categorizedData = data.map(d => ({
+        ...d,
+        weatherGroup: weatherCategories.includes(d.Weather_Condition_Cleaned) ? d.Weather_Condition_Cleaned : "Unknown"
+    }));
+
+    // Check
+    console.log("Weather Categories:", categorizedData.slice(0, 5).map(d => ({
+        Weather_Condition_Cleaned: d.Weather_Condition_Cleaned,
+        weatherGroup: d.weatherGroup
+    })));
+
+    const categories = d3.rollup(categorizedData,
+        v => d3.rollup(v,
+            values => values.length || 0,
+            d => d.year
+        ),
+        d => d.weatherGroup
+    );
+
+    // Check
+    console.log("Categories:", categories);
 
     // 3.a: SET SCALES FOR CHART 1
 
-
+    
     // 4.a: PLOT DATA FOR CHART 1
 
 
